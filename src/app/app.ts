1db25b454262b34367c5465c5ee6cd4721536845
nameIvanBond <kelebras@gmail.com>
2025-10-20 11:45:56 +0300

    first commit

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [RouterOutlet, MatButtonModule],
   templateUrl: './app.html',
-  styleUrl: './app.scss'
 })
 export class App {
   protected title = 'testOne';
